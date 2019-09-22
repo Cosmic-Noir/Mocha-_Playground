c27abcce9527605a74bf2e494ed3c00fb3eae080
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-22 14:16:04 -0600

    Add supertest for GET /sum endpoint

--- express_testing/test/app.test.js
+++ express_testing/test/app.test.js
@@ -9,3 +9,12 @@ describe("Express App", () => {
       .expect(200, "Hello Express!");
   });
 });
+
+describe("Get /sum", () => {
+  it("8/4 should be 2", () => {
+    return supertest(app)
+      .get("/sum")
+      .query({ a: 8, b: 4 })
+      .expect(200, "8 divided by 4 is 2");
+  });
+});
